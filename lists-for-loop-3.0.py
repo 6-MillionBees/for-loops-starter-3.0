2b0800f6eedff411197a8984c09539ac9c2dd602
6MillionBees <BoettcherAr321@northwested.org>
2024-11-11 10:49:09 -0500

    Add files via upload

---
+++ lists-for-loop-3.0.py
@@ -0,0 +1,25 @@
+# Arden Boettcher
+# 11/11/24
+# Lists For Loops 3.0
+
+
+# 4-1 Pizzas
+
+pizzas = ['Hawaiian', 'Meat Lover\'s', 'Pepperoni']
+
+for pizza in pizzas:
+    print(f'I love {pizza} pizza.')
+
+print('I think I just really like pizza.\n')
+
+
+
+# 4-2 Animals
+
+animals = ['Dog', 'Cat', 'Bird']
+
+for animal in animals:
+    print(f'A {animal} would make a great pet.')
+
+print('Honestly, most small animals would make good pets.')
+print('(Aside from the venomous ones)')
